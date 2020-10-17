dc1bcbdfeaa4355bb37284a389662cf4ac1be4fe
p-cuadros <patrick_jcq@hotmail.com>
2020-10-17 11:58:57 -0500

    v01.1

--- FinancieraCore2020.Dominio/Entidades/TipoMovimiento.cs
+++ FinancieraCore2020.Dominio/Entidades/TipoMovimiento.cs
@@ -1,16 +1,16 @@
 namespace FinancieraCore2020.Dominio.Entidades
 {
     /// <summary>
-    /// 
+    /// Clase que representa 
     /// </summary>
     public class TipoMovimiento
     {
         /// <summary>
-        /// 
+        /// Representa el identificador del tipo de movimiento
         /// </summary>
         public int IdTipo { get; private set; }
         /// <summary>
-        /// 
+        /// Representa la descripcion del tipo de movimiento
         /// </summary>
         public string DescripcionTipo { get; private set; }
     }
