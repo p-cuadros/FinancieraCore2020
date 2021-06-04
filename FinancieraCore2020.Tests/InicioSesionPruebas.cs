69c1c90a31b26719e3d197de96c2de9d36a97757
p-cuadros <patrick_jcq@hotmail.com>
2021-06-04 07:06:04 -0500

    librerias

--- FinancieraCore2020.Tests/InicioSesionPruebas.cs
+++ FinancieraCore2020.Tests/InicioSesionPruebas.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
 namespace FinancieraCore2020.Tests
 {
     public class InicioSesionPruebas
@@ -11,22 +14,38 @@ namespace FinancieraCore2020.Tests
         [SetUp]
         public void Iniciar()
         {
-            driver = new ChromeDriver();
+            //driver = new ChromeDriver();
+            var options = new ChromeOptions();
+            // options.AddArguments("--headless");
+            // options.AddArguments("--disable-gpu");
+            // options.AddArguments("--disable-dev-shm-usage");
+            // options.AddAdditionalCapability(CapabilityType.Version, "latest", true);
+            // options.AddAdditionalCapability(CapabilityType.Platform, "WIN10", true);
+            // options.AddAdditionalCapability("key", "key", true);
+            // options.AddAdditionalCapability("secret", "secret", true);
+            // options.AddAdditionalCapability("name", TestContext.CurrentContext.Test.Name, true);            
+            // if (driver == null){
+                //driver = new RemoteWebDriver(new Uri("http://127.0.0.1:4444/"), options.ToCapabilities(), TimeSpan.FromSeconds(600));
+            var capabilities = options.ToCapabilities();
+            var url = "http://localhost:4446/wd/hub";
+            driver = new RemoteWebDriver(new Uri(url), options);
+            //}
         }
 
         [Test]
         public void Probar()
         {
-            Thread.Sleep(5000);
+            Thread.Sleep(3000);
             //driver.Url = "http://www.google.com.pe";
             driver.Url = "http://oficinavirtual.epstacna.com.pe/src/vista/v_login.php";
-            driver.Manage().Window.Maximize();
-            Thread.Sleep(3000);
-            driver.FindElement(By.XPath("//*[@id='usu']")).SendKeys("");
-            Thread.Sleep(3000);
-            driver.FindElement(By.XPath("//*[@id='pas']")).SendKeys("");
-            Thread.Sleep(3000);
-            driver.FindElement(By.XPath("/html/body/div/div[2]/div/form/div[3]/div/button")).Click();
+            //driver.Navigate().GoToUrl("http://oficinavirtual.epstacna.com.pe/src/vista/v_login.php");
+            // driver.Manage().Window.Maximize();
+            // Thread.Sleep(3000);
+            // driver.FindElement(By.XPath("//*[@id='usu']")).SendKeys("1111");
+            // Thread.Sleep(3000);
+            // driver.FindElement(By.XPath("//*[@id='pas']")).SendKeys("1111");
+            // Thread.Sleep(3000);
+            // driver.FindElement(By.XPath("/html/body/div/div[2]/div/form/div[3]/div/button")).Click();
             Thread.Sleep(3000);
         }
 
